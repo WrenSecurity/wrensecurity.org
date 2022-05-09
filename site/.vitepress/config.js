e90693d0583158240542539e2f8cadb67bb4e442
Karel Maxa <karel.maxa@orchitech.cz>
2022-05-09 15:35:21 +0200

    Use root base.

--- site/.vitepress/config.js
+++ site/.vitepress/config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  base: '/wrensec-website/',
+  base: '/',
   title: 'Wren Security',
   description: 'The Wren Security Suite.',
   themeConfig: {
